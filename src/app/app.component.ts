7b075caadf254e2c326e04a84cfc752f208c2357
Joshua T J <joshuajose312@gmail.com>
2025-12-21 13:00:05 +0530

    feat: Enhance navbar visibility and styling on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import {
   ActivatedRoute,
@@ -7,7 +6,6 @@ import {
   RouterLinkActive,
   RouterOutlet,
 } from '@angular/router';
-import { share } from 'rxjs';
 import { FooterComponent } from './Components/footer/footer.component';
 import { OffcanvasComponent } from './Shared/components/offcanvas/offcanvas.component';
 
@@ -26,10 +24,34 @@ import { OffcanvasComponent } from './Shared/components/offcanvas/offcanvas.comp
 export class AppComponent {
   title = 'SGProEvents';
   currentRoute: string = '';
-  activeFragment;
+  // activeFragment;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
+    this.setSrcolltoTopVisiblity();
+    this.setNavBarVisiblity();
+  }
+
+  constructor(public route: ActivatedRoute, public router: Router) {
+    // this.activeFragment = this.route.fragment.pipe(share());
+  }
+
+  ngOnInit(): void {
+    // this.router.events.subscribe(() => {
+    //   this.setCurrentRoute();
+    // });
+    // this.setCurrentRoute();
+  }
+
+  // private setCurrentRoute(): void {
+  //   this.currentRoute = this.router.url.split('#')[0];
+  // }
+
+  scrollToTop() {
+    window.scrollTo(0, 0);
+  }
+
+  private setSrcolltoTopVisiblity() {
     const winScroll =
       document.body.scrollTop || document.documentElement.scrollTop;
     const height =
@@ -38,6 +60,7 @@ export class AppComponent {
     const scrolled = (winScroll / height) * 100;
 
     const scrollBtn = document.getElementById('goto-top');
+    const navBar = document.getElementById('nav-header');
     if (scrollBtn) {
       scrollBtn.style.setProperty('--scroll', `${scrolled}%`);
       if (winScroll > 400) {
@@ -47,21 +70,24 @@ export class AppComponent {
       }
     }
   }
+  private setNavBarVisiblity() {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
 
-  constructor(public route: ActivatedRoute, public router: Router) {
-    this.activeFragment = this.route.fragment.pipe(share());
-  }
-  ngOnInit(): void {
-    this.router.events.subscribe(() => {
-      this.setCurrentRoute();
-    });
-    this.setCurrentRoute();
-  }
-  private setCurrentRoute(): void {
-    this.currentRoute = this.router.url.split('#')[0];
-  }
-
-  scrollToTop() {
-    window.scrollTo(0, 0);
+    const navBar = document.getElementById('nav-header');
+    if (navBar) {
+      if (winScroll > 500) {
+        navBar.classList.add('nav-header-with-bg');
+        navBar.style.display = 'grid';
+      } else {
+        if (navBar.classList.contains('nav-header-with-bg')) {
+          navBar.classList.remove('nav-header-with-bg');
+        }
+      }
+    }
   }
 }
